16a882e75a877a44cd73aad6c22c893319225ba3
jkwendorf <jkwendorf>
2014-06-09 16:53:29 -0700

    Added code, but not ready for testing

--- TowerDefense/Assets/Scripts/NetworkHelper.cs
+++ TowerDefense/Assets/Scripts/NetworkHelper.cs
@@ -1,15 +1,261 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿
+using System; 
+using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic;
+using MiniJSON;
 
+/** This file is 
+ * VERSION: 1.0 6/9/2014 11:00am
+ */
 public class NetworkHelper : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	/* This is the game server for the final project */
+	string REST_URL = "http://djp3-pc2.ics.uci.edu:9021";
+	//string REST_URL = "http://localhost:9021";
+	//string REST_URL = "http://128.195.59.193:9021";
+	
+	/* This is a generic helper class for a 3D point */
+	private class Point{
+		public double lat;
+		public double lng;
+		public double alt;
+		
+		public Point(double lat, double lng,double alt){
+			this.lat = lat;
+			this.lng = lng;
+			this.alt = alt;
+		}
+		
+		public double getLat(){
+			return lat;
+		}
+		
+		public double getLng(){
+			return lng;
+		}
+		
+		public double getAlt(){
+			return alt;
+		}
+		
+		
+		public Dictionary<string,string> toDictionary(){
+			Dictionary<string,string> ret = new Dictionary<string,string> ();
+			ret.Add ("lat", this.lat + "");
+			ret.Add ("lng", this.lng + "");
+			ret.Add ("alt", this.alt + "");
+			return ret;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	/* These are variable internal to MyNetworkHelper.  Access them through getters and setters */
+	private Point towerPoint = null;
+	private Point bombPoint = null;
+	private string storedCode = null;
 	
+	/* This is how you store a point at which you'd like to build a tower.  This doesn't upload that point */
+	public void buildTowerPoint(double lat, double lng, double alt){
+		Debug.Log ("buildTowerPoint called");
+		towerPoint = new Point (lat, lng, alt);
+	}
+	
+	/* This tells you if you have a tower point to be uploaded */
+	public bool isTowerSet(){
+		return towerPoint != null;
+	}
+	
+	public double getTowerLat(){
+		return towerPoint.getLat();
+	}
+	
+	public double getTowerLng(){
+		return towerPoint.getLng();
+	}
+	
+	public double getTowerAlt(){
+		return towerPoint.getAlt();
+	}
+	
+	/* This is how you store a point at which you'd like to drop a bomb.  This doesn't upload that point */
+	public void dropBombPoint(double lat, double lng, double alt){
+		Debug.Log ("addTowerPoint called");
+		bombPoint = new Point (lat, lng, alt);
+	}
+	
+	/* This tells you if you have a bomb point to be uploaded */
+	public bool isBombSet(){
+		return bombPoint != null;
+	}
+	
+	public double getBombLat(){
+		return bombPoint.getLat();
+	}
+	
+	public double getBombLng(){
+		return bombPoint.getLng();
+	}
+	
+	public double getBombAlt(){
+		return bombPoint.getAlt();
+	}
+	
+	/* This is how you store a power up code for later upload.  This doesn't upload that power up code */
+	public void enterCode(string code){
+		Debug.Log ("enterCode called");
+		storedCode = code;
+	}
+	
+	
+	/* This executes an upload of the already stored Tower point,  the tower point will be cleared after successful */
+	public Dictionary<string,object> uploadTowerPoint(string worldName,string worldPassword,string playerName,string playerPassword){
+		WWW www;
+		
+		if (towerPoint != null){				
+			string u = REST_URL+"/build_tower";
+			u += "?world_name=" + WWW.EscapeURL (worldName);
+			u += "&world_password=" + WWW.EscapeURL (worldPassword);
+			u += "&player_name=" + WWW.EscapeURL (playerName);
+			u += "&player_password=" + WWW.EscapeURL (playerPassword);
+			u += "&lat=" + WWW.EscapeURL (towerPoint.lat+"");
+			u += "&lng=" + WWW.EscapeURL (towerPoint.lng+"");
+			u += "&alt=" + WWW.EscapeURL (towerPoint.alt+"");
+			www = new WWW (u);
+			StartCoroutine (WaitForRequest (www));
+			if(www.error == null){
+				//Everything went ok
+				towerPoint = null;
+				var dict = Json.Deserialize(www.text) as Dictionary<string,object>;
+				return dict;
+			}
+			else{
+				var dict = new Dictionary<string,object>();
+				dict.Add("error","true");
+				return dict;
+			}
+		}
+		var ret = new Dictionary<string,object>();
+		ret.Add("error","false");
+		return ret;
+	}
+	
+	
+	/* This executes an upload of the already stored bomb point,  the bomb point will be cleared after successful upload*/
+	public Dictionary<string,object> uploadBombPoint(string worldName,string worldPassword,string playerName,string playerPassword){
+		WWW www;
+		
+		if (bombPoint != null){				
+			string u = REST_URL+"/drop_bomb";
+			u += "?world_name=" + WWW.EscapeURL (worldName);
+			u += "&world_password=" + WWW.EscapeURL (worldPassword);
+			u += "&player_name=" + WWW.EscapeURL (playerName);
+			u += "&player_password=" + WWW.EscapeURL (playerPassword);
+			u += "&lat=" + WWW.EscapeURL (bombPoint.lat+"");
+			u += "&lng=" + WWW.EscapeURL (bombPoint.lng+"");
+			u += "&alt=" + WWW.EscapeURL (bombPoint.alt+"");
+			www = new WWW (u);
+			StartCoroutine (WaitForRequest (www));
+			if(www.error == null){
+				//Everything went ok
+				bombPoint = null;
+				var dict = Json.Deserialize(www.text) as Dictionary<string,object>;
+				return dict;
+			}
+			else{
+				var dict = new Dictionary<string,object>();
+				dict.Add("error","true");
+				return dict;
+			}
+		}
+		var ret = new Dictionary<string,object>();
+		ret.Add("error","false");
+		return ret;
+	}
+	
+	
+	
+	/* This executes an upload of the already stored power up code,  the code will be cleared after successful upload*/
+	public Dictionary<string,object> uploadCode(string worldName,string worldPassword,string playerName,string playerPassword){
+		WWW www;
+		
+		if (storedCode != null){				
+			string u = REST_URL+"/redeem_power_up";
+			u += "?world_name=" + WWW.EscapeURL (worldName);
+			u += "&world_password=" + WWW.EscapeURL (worldPassword);
+			u += "&player_name=" + WWW.EscapeURL (playerName);
+			u += "&player_password=" + WWW.EscapeURL (playerPassword);
+			u += "&code=" + WWW.EscapeURL (storedCode);
+			www = new WWW (u);
+			StartCoroutine (WaitForRequest (www));
+			if(www.error == null){
+				//Everything went ok
+				storedCode = null;
+				var dict = Json.Deserialize(www.text) as Dictionary<string,object>;
+				return dict;
+			}
+			else{
+				var dict = new Dictionary<string,object>();
+				dict.Add("error","true");
+				return dict;
+			}
+		}
+		var ret = new Dictionary<string,object>();
+		ret.Add("error","false");
+		return ret;
+	}
+	
+	
+	/* This requests the state of the game world from the server.  It requires a callback that will be called when the data arrives.
+	 * The signature of the callback function should be like:
+	 * 	private void callback(Dictionary<string,object> state){}
+	 * when that function is called back it will receive a Dictionary with data about the world map. */
+	public void refreshGameState(string worldName,string worldPassword,string playerName,string playerPassword,Action<Dictionary<string,object>> callback){
+		WWW www;
+		
+		
+		string u = REST_URL+"/get_game_state";
+		u += "?world_name=" + WWW.EscapeURL (worldName);
+		u += "&world_password=" + WWW.EscapeURL (worldPassword);
+		u += "&player_name=" + WWW.EscapeURL (playerName);
+		u += "&player_password=" + WWW.EscapeURL (playerPassword);
+		www = new WWW (u);
+		StartCoroutine(WaitForGameStateRequest (www,callback));
+		
+	}
+	
+	
+	private IEnumerator WaitForGameStateRequest (WWW www,Action<Dictionary<string,object>> callback)
+	{
+		yield return www;
+		
+		
+		// check for errors
+		if (www.error == null) {
+			//Debug.Log ("WWW Ok!: " + www.text);
+			//Everything went ok
+			var dict = Json.Deserialize(www.text) as Dictionary<string,object>;
+			callback(dict);
+		} else {
+			//Debug.Log ("WWW Error: " + www.error);
+			var dict = new Dictionary<string,object>();
+			dict.Add("error","true");
+			callback(dict);
+		}   
+		
+		
+	}
+	
+	
+	
+	private IEnumerator WaitForRequest (WWW www)
+	{
+		yield return www;
+		
+		// check for errors
+		if (www.error == null) {
+			Debug.Log ("WWW Ok!: " + www.text);
+		} else {
+			Debug.Log ("WWW Error: " + www.error);
+		}    
 	}
 }
--- TowerDefense/Assets/Scripts/myGUIScript.cs
+++ TowerDefense/Assets/Scripts/myGUIScript.cs
@@ -5,11 +5,18 @@ public class myGUIScript : MonoBehaviour {
 
 	public GUISkin guiSkin;
 
+
+	private string current_location;
+	private string tower_location;
+	private string bomb_location;
+
 	private string world_name;
 	private string world_password;
 	private	string team_name;
 	private string team_password;
 	private string code_command;
+
+	private bool isExpanded = false;
 
 	private NetworkHelper network_helper;
 
@@ -25,6 +32,10 @@ public class myGUIScript : MonoBehaviour {
 
 		network_helper = GameObject.Find("myNetworkHelper").GetComponentInChildren<NetworkHelper>();
 
+		current_location = "Current Location: ";
+		tower_location = "Tower Location: ";
+		bomb_location = "Bomb Location: ";
+
 		world_name = "World Name";
 		world_password = "World Password";
 		team_name = "Team Name";
@@ -34,32 +45,84 @@ public class myGUIScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnGUI(){
 
 		GUI.skin = guiSkin;
 
-		world_name = GUI.TextField(new Rect(0, 0, Screen.width, 100), world_name);
-		world_password = GUI.TextField(new Rect(0, 100, Screen.width, 100), world_password);
-		team_name = GUI.TextField(new Rect(0, 200, Screen.width, 100), team_name);
-		team_password = GUI.TextField(new Rect(0, 300, Screen.width, 100), team_password);
-		code_command = GUI.TextField(new Rect(0, 400, Screen.width, 100), code_command);
-
-		if(GUI.Button(new Rect(0, 500, Screen.width, 100), "Place Tower"))
+		if(GUI.Button(new Rect(0, 0, Screen.width/2, 100), "Fetch Status"))
 		{
-			// Place a tower on the map
+			// Get the status from the Network Helper
+			//network_helper.refreshGameState(world_name, world_password, team_name, team_password, );
+
+			// With the received data, update the current state of our game
 		}
 
-		if(GUI.Button(new Rect(0, 600, Screen.width, 100), "Place Bomb"))
+		isExpanded = GUI.Toggle( new Rect(Screen.width/2, 0, Screen.width/2, 100), isExpanded, "Expand");
+		
+		if(isExpanded)
 		{
-			// Place a bomb on the map at our coordinate
-		}
+			// Draw all the other info
 
-		if(GUI.Button(new Rect(0, Screen.height - 100, Screen.width, 100), "Upload"))
-		{
-			// Upload our input to the server
+			current_location = GUI.TextField(new Rect(0, 100, Screen.width, 100), current_location);
+			tower_location = GUI.TextField(new Rect(0, 200, Screen.width, 100), tower_location);
+			bomb_location = GUI.TextField(new Rect(0, 300, Screen.width, 100), bomb_location);
+
+			world_name = GUI.TextField(new Rect(0, 400, Screen.width/2, 100), world_name);
+			world_password = GUI.TextField(new Rect(0, 500, Screen.width/2, 100), world_password);
+			team_name = GUI.TextField(new Rect(0, 600, Screen.width/2, 100), team_name);
+			team_password = GUI.TextField(new Rect(0, 700, Screen.width/2, 100), team_password);
+			code_command = GUI.TextField(new Rect(0, 800, Screen.width/2, 100), code_command);
+
+			if(GUI.Button(new Rect(Screen.width/2, 400, Screen.width/2, 100), "Place Tower"))
+			{
+				// Place a tower on the map
+				//network_helper.buildTowerPoint(lat, long, alt);
+
+				// Set the name of the tower location
+				//tower_location = "Tower Location: (" + lat + "," + long + "," + alt + ")";
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2, 500, Screen.width/2, 100), "Upload Tower"))
+			{
+				// Upload our input to the server and save the dictionary
+				//network_helper.uploadTowerPoint(world_name, world_password, team_name, team_password);
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2, 600, Screen.width/2, 100), "Place Bomb"))
+			{
+				// Place a bomb on the map at our coordinate
+				//network_helper.dropBombPoint(lat, long, alt);
+				
+				// Set the name of the bomb location
+				//tower_location = "Tower Location: (" + lat + "," + long + "," + alt + ")";
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2, 700, Screen.width/2, 100), "Upload Bomb"))
+			{
+				// Upload our input to the server and save the dictionary
+				//network_helper.uploadBombPoint (world_name, world_password, team_name, team_password);
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2, 800, Screen.width/2, 100), "Store Code"))
+			{
+				// Save the code that the user entered
+
+				if(!code_command.Equals ("Code Command"))
+				{
+					//network_helper.enterCode(code_command);
+
+					code_command = "Code Command";
+				}
+			}
+
+			if(GUI.Button(new Rect(Screen.width/2, 900, Screen.width/2, 100), "Upload Code"))
+			{
+				// Upload our code and save the dictionary
+				//network_helper.uploadCode (world_name, world_password, team_name, team_password);
+			}
 		}
 	}
 }
